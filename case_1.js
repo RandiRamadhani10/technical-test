37565195ee7650c88244957599742f8e742ba326
RandiRamadhani10 <80814841+RandiRamadhani10@users.noreply.github.com>
2023-01-06 21:08:31 +0700

    first commit

---
+++ case_1.js
@@ -0,0 +1,58 @@
+const fruits = [
+  {
+    fruitId: 1,
+    fruitName: "Apel",
+    fruitType: "IMPORT",
+    stock: 10,
+  },
+  {
+    fruitId: 2,
+    fruitName: "Kurma",
+    fruitType: "IMPORT",
+    stock: 20,
+  },
+  {
+    fruitId: 3,
+
+    fruitName: "apel",
+    fruitType: "IMPORT",
+    stock: 50,
+  },
+  {
+    fruitId: 4,
+    fruitName: "Manggis",
+    fruitType: "LOCAL",
+    stock: 100,
+  },
+  {
+    fruitId: 5,
+    fruitName: "Jeruk Bali",
+    fruitType: "LOCAL",
+    stock: 10,
+  },
+  {
+    fruitId: 5,
+    fruitName: "KURMA",
+    fruitType: "IMPORT",
+    stock: 20,
+  },
+  {
+    fruitId: 5,
+    fruitName: "Salak",
+    fruitType: "LOCAL",
+    stock: 150,
+  },
+];
+
+let fruitName = "Buah yang dimiliki andi : ";
+let fruitNameFilter = [];
+fruits.forEach((data) => {
+  if (data.fruitName !== "apel") {
+    if (data.fruitName !== "KURMA") {
+      return (fruitName += ` ${data.fruitName},`);
+    }
+    return (fruitName += ` ${data.fruitName},`);
+  } else {
+  }
+});
+console.log(fruitName);
